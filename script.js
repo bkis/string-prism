3b032636d7a4cf0efcab9165d5cab5129a498e7a
bkis <kiss@mailbox.org>
2020-07-24 10:46:53 +0200

    doc

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+// this is science, everything is fully intended
 function getColorChannelValue(from, modifier){
     return 20 + parseInt((((from * 7251) % (42 + modifier)) / (42 + modifier)) * 235, 10) * 0.75;
 }
 
+// construct the result element for the given input
 function getResultElement(input){
     let element = $("<div/>");
+    // iterate chars
     input.split('').forEach((c,i) => {
+        // calculate color
         let r = getColorChannelValue(c.codePointAt(0), 113);
         let g = getColorChannelValue(c.codePointAt(0), 22);
         let b = getColorChannelValue(c.codePointAt(0), 358);
         let color = "rgba(" + r + "," + g + "," + b + ", 1.0)";
+        // prepare codes for display on page
         let codes = c.codePointAt(0).toString(16).toUpperCase() +
             "<br>" + c.codePointAt(0);
+        // prepare codes for link tooltips
         let codesTitle = "JS Unicode: \\u" + c.codePointAt(0).toString(16).toUpperCase().padStart(4, "0") +
         "\nHTML entity: &amp;#" + c.codePointAt(0) + "\;";
+        // prepare link anchor element
         let charLink = $(
             "<a href='https://codepoints.net/U+" + 
             c.codePointAt(0).toString(16) + 
             "' target='_blank' " +
             "title='" + codesTitle + "' />");
-        let char = $("<div class='char'/>").css("background-color", color);
+        // put it all together
+        let char = $("<div class='char box-shadow'/>").css("background-color", color);
         charLink.append(char);
         char.append($("<div class='enc'/>").text(c));
         char.append($("<div class='codepoint'/>").html("<b>" + i + "</b>" + codes));
@@ -49,13 +57,16 @@ function processInput(){
 }
 
 $(function() {
+    // submit event
     $("#input-form").submit(function(e) {
         e.preventDefault();
         processInput();
     });
+    // input event
     $("#input").on("input", function() {
         processInput();
     });
+    // set and process placeholder input
     $("#input").val("Et Voilà!");
     processInput();
 });
